eede6a3fe77b2a76ea6053b35f1b6440d297296a
stevendegreat <stevendenjuguna@gmail.com>
2023-07-14 00:21:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"ith proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
