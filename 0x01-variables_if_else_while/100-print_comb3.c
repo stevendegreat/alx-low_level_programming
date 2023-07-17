2fa69b474d77190c9c91ab6b479f520df67e58d7
stevendegreat <stevendenjuguna@gmail.com>
2023-07-17 10:37:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-int main(void) {
-    int i = 0;
-    int j = 0;
-    
-    while (i <= 9) {
-        j = 0;
-        
-        while (j <= 9) {
-            if (i != j && i < j) {
-                putchar(i + '0');
-                putchar(j + '0');
-                
-                if (i == 8 && j == 9) {
-                    putchar('\n');
-                } else {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            
-            j++;
-        }
-        
-        i++;
-    }
-    
-    return 0;
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 9)
+	{
+		j = 0;
+
+		while (j <= 9)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if (i == 8 && j == 9)
+					putchar('\n');
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return 0;
 }
