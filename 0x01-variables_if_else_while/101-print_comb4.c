93beb1878a2c3867f86ea73b17fa9a4344016c41
stevendegreat <stevendenjuguna@gmail.com>
2023-07-17 23:13:36 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,28 +10,28 @@
  */
 int main(void)
 {
-    int digit1, digit2, digit3; /* Variables to store the digits */
+	int digit1, digit2, digit3; /* Variables to store the digits */
 
-    for (digit1 = 0; digit1 <= 9; digit1++)
-    {
-        for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-        {
-            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-            {
-                putchar(digit1 + '0'); /* Print first digit */
-                putchar(digit2 + '0'); /* Print second digit */
-                putchar(digit3 + '0'); /* Print third digit */
+	for (digit1 = 0; digit1 <= 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+			{
+				putchar(digit1 + '0'); /* Print first digit */
+				putchar(digit2 + '0'); /* Print second digit */
+				putchar(digit3 + '0'); /* Print third digit */
 
-                if (digit1 < 7 || digit2 < 8 || digit3 < 9)
-                {
-                    putchar(','); /* Print comma */
-                    putchar(' '); /* Print space */
-                }
-            }
-        }
-    }
+				if (digit1 < 7 || digit2 < 8 || digit3 < 9)
+				{
+					putchar(','); /* Print comma */
+					putchar(' '); /* Print space */
+				}
+			}
+		}
+	}
 
-    putchar('\n'); /* Print newline */
+	putchar('\n'); /* Print newline */
 
-    return (0);
+	return (0);
 }
