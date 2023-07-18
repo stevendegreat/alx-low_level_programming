99a3f8a7f781f18c88961ea30e08a683ff19a9b4
stevendegreat <stevendenjuguna@gmail.com>
2023-07-17 23:24:49 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,30 +10,30 @@
  */
 int main(void)
 {
-    int num1, num2; /* Variables to store the numbers */
+	int num1, num2; /* Variables to store the numbers */
 
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = num1; num2 <= 99; num2++)
-        {
-            if (num1 == num2)
-                continue;
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			if (num1 == num2)
+				continue;
 
-            putchar((num1 / 10) + '0'); /* Print tens digit of num1 */
-            putchar((num1 % 10) + '0'); /* Print units digit of num1 */
-            putchar(' '); /* Print space */
-            putchar((num2 / 10) + '0'); /* Print tens digit of num2 */
-            putchar((num2 % 10) + '0'); /* Print units digit of num2 */
+			putchar((num1 / 10) + '0'); /* Print tens digit of num1 */
+			putchar((num1 % 10) + '0'); /* Print units digit of num1 */
+			putchar(' '); /* Print space */
+			putchar((num2 / 10) + '0'); /* Print tens digit of num2 */
+			putchar((num2 % 10) + '0'); /* Print units digit of num2 */
 
-            if (num1 < 98 || num2 < 99)
-            {
-                putchar(','); /* Print comma */
-                putchar(' '); /* Print space */
-            }
-        }
-    }
+			if (num1 < 98 || num2 < 99)
+			{
+				putchar(','); /* Print comma */
+				putchar(' '); /* Print space */
+			}
+		}
+	}
 
-    putchar('\n'); /* Print newline */
+	putchar('\n'); /* Print newline */
 
-    return (0);
+	return (0);
 }
