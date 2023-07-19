8afe3e412b0bfbe0df15f702dd09ab624a909c35
stevendegreat <stevendenjuguna@gmail.com>
2023-07-19 03:02:23 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
-int _putchar(char c) {
-    return write(1, &c, 1); // Write the character to the standard output (file descriptor 1)
-}
 
-void print_alphabet(void) {
-    /* Your implementation of print_alphabet */
-    /* ... */
-}
+/**
+ * main -Entry point of the code,
+ *
+ * Return: Always 0
+ */
 
-// Implement other functions following the same pattern
-// ...
-
-int main(void) {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,18 @@
-#include "main.h"
-#include <unistd.h> // Include the necessary library for the write syscall
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
-int _putchar(char c) {
-    return write(1, &c, 1); // Write the character to the standard output (file descriptor 1)
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-// Implement other functions following the same pattern
-// ...
-
+#endif
