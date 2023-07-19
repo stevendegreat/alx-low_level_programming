d67616644dad4579b92cfd72c51d49b91aab87db
stevendegreat <stevendenjuguna@gmail.com>
2023-07-19 13:05:53 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,10 @@
  */
 int _islower(int c)
 {
-    if (c >= 97 && c <= 122)
-    {
-        return (1);
-    }
-    return (0);
+	/* Using tabs for indentation instead of spaces */
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
 }
