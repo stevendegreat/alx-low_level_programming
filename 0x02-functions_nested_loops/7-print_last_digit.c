f74b2e3d477e101c733f6cea2d9c56a5a6c847df
stevendegreat <stevendenjuguna@gmail.com>
2023-07-19 22:42:25 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
 int print_last_digit(int n)
 
 {
-        int last;
+int last;
 
-        last = n % 10;
-        if (last < 0)
+last = n % 10;
+if (last < 0)
 {
-        last = last * -1;
+last = last * -1;
 }
-        _putchar(last + '0');
-        return (last);
+_putchar(last + '0');
+return (last);
 }
