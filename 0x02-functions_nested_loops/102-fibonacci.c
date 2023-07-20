5d38b91dda2f9935b07b5cec72e23bf3e3bb94c7
stevendegreat <stevendenjuguna@gmail.com>
2023-07-20 05:04:07 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - print the first 52 fibonacci numbers
+ * main - print the sum of all multiples of 3 or 5 up to 1024
  * Return: Always 0 (success)
  */
 int main(void)
 {
-    int i = 0;
-    long j = 1, k = 2;
+int i, z = 0;
 
-    while (i < 51) // Changed loop condition to i < 51
-    {
-        if (i == 0)
-            printf("%ld", j);
-        else if (i == 1)
-            printf(", %ld", k);
-        else
-        {
-            k += j;
-            j = k - j;
-            printf(", %ld", k);
-        }
-        ++i;
-    }
-
-    printf("\n");
-    return (0);
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
 }
+}
+
+ printf("%d\n", z);
+return (0);
+}
+
