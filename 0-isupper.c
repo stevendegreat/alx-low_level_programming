55f78c89eeb9842552c72657a3a30a86b986e2d4
stevendegreat <stevendenjuguna@gmail.com>
2023-07-21 01:24:55 -0700

    more nested loops

---
+++ 0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
