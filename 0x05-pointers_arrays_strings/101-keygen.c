5c73762348500b07760bba54e3715680e075607f
stevendegreat <stevendenjuguna@gmail.com>
2023-07-25 04:57:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,53 +3,32 @@
 #include <time.h>
 
 /**
- * main - Generates a random valid password for the program 101-crackme.
+ * main - Generates the fixed valid password for the program 101-crackme.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char password[12];
-    int index = 0, sum = 0, diff_half1, diff_half2;
+    char password[] = "Tada! Congrats";
+    int sum = 0, i;
 
     srand(time(0));
 
-    /* Generate the password */
-    while (sum < 101)
+    /* Calculate the sum of the characters in the fixed password */
+    for (i = 0; password[i]; i++)
     {
-        password[index] = 33 + rand() % 94;
-        sum += password[index++];
-    }
-    password[index] = '\0';
-
-    /* Adjust the password to get the desired sum */
-    if (sum != 101)
-    {
-        diff_half1 = (sum - 101) / 2;
-        diff_half2 = (sum - 101) / 2;
-
-        if ((sum - 101) % 2 != 0)
-            diff_half1++;
-
-        for (index = 0; password[index]; index++)
-        {
-            if (password[index] >= (33 + diff_half1))
-            {
-                password[index] -= diff_half2;
-                break;
-            }
-        }
-
-        for (index = 0; password[index]; index++)
-        {
-            if (password[index] >= (33 + diff_half2))
-                password[index] -= diff_half2;
-            break;
-        }
+        sum += password[i];
     }
 
-    /* Print the generated password */
-    printf("%s\n", password);
+    /* Generate additional random characters to reach the desired sum */
+    while (sum < 2772)
+    {
+        char random_char = 33 + rand() % 94;
+        sum += random_char;
+        putchar(random_char);
+    }
+
+    putchar('\n');
 
     return (0);
 }
