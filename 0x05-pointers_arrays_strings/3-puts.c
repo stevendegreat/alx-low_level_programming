737a39ad6c7191cfcdeb2539c9d0089be111cf67
stevendegreat <stevendenjuguna@gmail.com>
2023-07-25 01:11:09 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,15 +6,16 @@
  *
  * Return: void
  */
+
 void _puts(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
