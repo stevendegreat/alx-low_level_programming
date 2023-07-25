769cdcc56e29a3f2f308e25dc89f42db9cc61fff
stevendegreat <stevendenjuguna@gmail.com>
2023-07-25 04:40:36 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,10 +4,13 @@
 
 /**
  * main - generates random valid passwords for the
- * program 101-crackme.
+ *        program 101-crackme.
+ *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
 char password[84];
 int index = 0, sum = 0, diff_half1, diff_half2;
@@ -16,32 +19,47 @@ srand(time(0));
 
 while (sum < 2772)
 {
+
 password[index] = 33 + rand() % 94;
 sum += password[index++];
+
 }
 password[index] = '\0';
+
 if (sum != 2772)
 {
+
 diff_half1 = (sum - 2772) / 2;
 diff_half2 = (sum - 2772) / 2;
+
 if ((sum - 2772) % 2 != 0)
 diff_half1++;
+
 for (index = 0; password[index]; index++)
 {
+
 if (password[index] >= (33 + diff_half1))
 {
+
 password[index] -= diff_half2;
 break;
 }
 }
+
 for (index = 0; password[index]; index++)
 {
+
 if (password[index] >= (33 + diff_half2))
 password[index] -= diff_half2;
 break;
 }
 }
-printf("%s", password);
+
+if (strcmp(password, "Tada! Congrats") == 0)
+printf("Correct password: %s\n", password);
+else
+printf("Wrong password: %s\n", password);
+
 
 return (0);
 }
