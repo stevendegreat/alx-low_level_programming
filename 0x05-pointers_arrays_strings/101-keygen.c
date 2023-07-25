c26eb562daabeb86d929bcd72fb4208d55ff8e10
stevendegreat <stevendenjuguna@gmail.com>
2023-07-25 05:08:03 -0700

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,44 @@
-#include <stdio.h>
+#include <time.h>
 
 /**
- * main - Generates the fixed valid password for the program 101-crackme.
- *
+ * main - generates random valid passwords for the
+ * program 101-crackme.
  * Return: Always 0.
  */
 int main(void)
 {
-    char password[] = "Tada! Congrats";
-    int i;
+        char password[84];
+        int index = 0, sum = 0, diff_half1, diff_half2;
 
-    /* Calculate the sum of the characters in the fixed password */
-    int sum = 0;
-    for (i = 0; password[i]; i++)
-    {
-        sum += password[i];
-    }
+        srand(time(0));
 
-    /* Adjust the last character of the password to reach the desired sum */
-    password[i - 1] = 2772 - sum;
-
-    /* Print the generated password */
-    printf("%s\n", password);
-
-    return (0);
+        while (sum < 2772)
+        {
+                password[index] = 33 + rand() % 94;
+                sum += password[index++];
+        }
+        password[index] = '\0';
+        if (sum != 2772)
+        {
+                diff_half1 = (sum - 2772) / 2;
+                diff_half2 = (sum - 2772) / 2;
+                if ((sum - 2772) % 2 != 0)
+                diff_half1++;
+                for (index = 0; password[index]; index++)
+                {
+                if (password[index] >= (33 + diff_half1))
+                {
+                        password[index] -= diff_half2;
+                        break;
+                }
+                }
+                for (index = 0; password[index]; index++)
+                {
+                        if (password[index] >= (33 + diff_half2))
+                        password[index] -= diff_half2;
+                        break;
+                }
+        }
+printf("%s", password);
+return (0);
 }
