809632aa2eac2597db3a60cc78406932311868c7
stevendegreat <stevendenjuguna@gmail.com>
2023-07-26 08:45:33 -0700

    debugging

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 
-/**
-* main - Entry point of the program
-*
-* Description: This program prints a[2] = 98.
-*
-* Return: Always 0 (success)
-*/
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+    int n;
+    int a[5];
+    int *p;
 
-a[2] = 1024;
-p = &n;
-
-/*
-* Write your line of code here...
-* Remember:
-* - You are not allowed to use 'a'
-* - You are not allowed to modify 'p'
-* - Only one statement
-* - You are not allowed to code anything else than this line of code
-*/
-*(p + 2) = 98;
-
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-
-return (0);
+    a[2] = 1024;
+    p = &n;
+    /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
+     */
+    a[2] = 98; // Add this line to assign 98 to a[2]
+    /* ...so that this prints 98\n */
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
