b0662a161be5e2bbc0cdb522849720d78dfe86a9
stevendegreat <stevendenjuguna@gmail.com>
2023-07-28 06:02:35 -0700

    pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,17 +7,17 @@
  * Return: Pointer to the resulting string.
  */
 
-char cap_string(char str)
+char *cap_string(char *str)
 {
 int i = 0;
 
-/ Capitalize the first character if it's a lowercase letter /
+/* Capitalize the first character if it's a lowercase letter */
 if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
 
 while (str[i] != '\0')
 {
-/ Check for word separators and capitalize next letter /
+/* Check for word separators and capitalize next letter */
 if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 str[i] == '!' || str[i] == '?' || str[i] == '"' ||
@@ -25,7 +25,7 @@ str[i] == '(' || str[i] == ')' || str[i] == '{' ||
 str[i] == '}')
 {
 i++;
-/ Capitalize the next letter if it's a lowercase letter /
+/* Capitalize the next letter if it's a lowercase letter */
 if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
 }
