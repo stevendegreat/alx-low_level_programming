929e7ea302a87cda657ce32f96795813d560bb30
stevendegreat <stevendenjuguna@gmail.com>
2023-07-28 06:38:18 -0700

    pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,15 @@
  *
  * Return: Integer value less than 0 if s1 is less than s2,Integer value greater than 0 if s1 is greater than s2,0 if the strings are equal.
  */
-
 int _strcmp(char *s1, char *s2)
 {
 int counter = 0;
 int compare_value;
-
 while (s1[counter] == s2[counter] && s1[counter] != '\0')
 {
 counter++;
 }
-
 compare_value = s1[counter] - s2[counter];
+
 return compare_value;
 }
