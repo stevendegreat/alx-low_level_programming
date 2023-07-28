660b32b104cf7a3f52703851b044f481c025c3c8
stevendegreat <stevendenjuguna@gmail.com>
2023-07-27 23:21:36 -0700

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,54 @@
 #include "main.h"
 
 /**
+ * _putchar - Custom putchar function.
+ * @c: The character to be printed.
+ *
+ * Return: On success, return the character printed.
+ * On error, return -1 and set errno.
+ */
+
+int _putchar(char c)
+{
+    /* Implement the custom _putchar function here */
+    /* For example: */
+    return write(1, &c, 1);
+}
+
+/**
  * cap_string - Capitalizes all words of a string.
  * @str: The input string.
  *
  * Return: Pointer to the resulting string.
  */
-char cap_string(char str)
-{
-int i = 0;
 
-/ Capitalize the first character if it's a lowercase letter /
-if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
+char *cap_string(char *str)
+{
+    int i = 0;
 
-while (str[i] != '\0')
-{
-/ Check for word separators and capitalize next letter /
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}')
-{
-i++;
-/ Capitalize the next letter if it's a lowercase letter /
-if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
-}
-else
-{
-i++;
-}
-}
+    /* Capitalize the first character if it's a lowercase letter */
+    if (str[i] >= 'a' && str[i] <= 'z')
+        str[i] -= 32;
 
-return (str);
+    while (str[i] != '\0')
+    {
+        /* Check for word separators and capitalize next letter */
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+            str[i] == '}')
+        {
+            i++;
+            /* Capitalize the next letter if it's a lowercase letter */
+            if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] -= 32;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return str;
 }
