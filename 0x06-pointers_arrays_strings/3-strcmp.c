859673da65dcf774933af995af7ec42b96091f5c
stevendegreat <stevendenjuguna@gmail.com>
2023-07-28 04:00:35 -0700

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,13 +6,12 @@
  * @s1: Pointer to the first string.
  * @s2: Pointer to the second string.
  *
- * Return: Value less than 0 if s1 is less than s2,
- *         Value greater than 0 if s1 is greater than s2,
- *         0 if strings are equal.
+ * Return: Value less than 0 if s1 is less than s2,Value greater than 0 if s1 is greater than s2,0 if strings are equal.
  */
+
 int _strcmp(char *s1, char *s2)
 {
-    int counter = 0;
+int counter = 0;
 int compare_value;
 
 while (s1[counter] == s2[counter] && s1[counter] != '\0')
