55473d07d2f36b7c38666df4ebb7c5b1c269d50a
stevendegreat <stevendenjuguna@gmail.com>
2023-07-28 19:25:24 -0700

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies a string.
- * @dest: Destination string pointer.
- * @src: Source string pointer.
- * @n: Number of bytes to be used.
- *
- * Return: Pointer to destination string.
- */
+* _strncpy - Copies a string.
+* @dest: Destination string pointer.
+* @src: Source string pointer.
+* @n: Number of bytes to be used.
+*
+* Return: Pointer to destination string dest.
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 int count_of_bytes;
@@ -19,5 +19,6 @@ for (; count_of_bytes < n; count_of_bytes++)
 {
 dest[count_of_bytes] = '\0';
 }
+/* Break the line to avoid exceeding 80 characters */
 return (dest);
 }
