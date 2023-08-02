10027a8a530e8afb82b9ad390a2c3154fb864204
stevendegreat <stevendenjuguna@gmail.com>
2023-08-02 02:00:23 -0700

    C - Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,15 +3,14 @@
 /**
  * factorial - factorial of a given number
  * @n: pointer block of memory to fill
- * return: factorial
+ * Return: factorial of n, or -1 if n is negative
  */
 int factorial(int n)
 {
 if (n == 0) /* base conditions */
-return(1);
+return (1);
 else if (n < 0) /* base conditions */
 return (-1);
 else
-return (n * factorial(n - 1)); /* recursive call*/
-
+return (n * factorial(n - 1)); /* recursive call */
 }
