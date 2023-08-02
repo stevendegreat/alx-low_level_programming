e52dc40fa6f825d4d922c64e007e726e5157fe0b
stevendegreat <stevendenjuguna@gmail.com>
2023-08-02 00:55:32 -0700

    C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0') /* check if string is not empty */
+{
+_putchar(*s); /* print the first character */
+_puts_recursion(s + 1); /* call the function recursively with the next character */
+}
+else
+{
+_putchar('\n'); /* print a new line */
+}
+}
