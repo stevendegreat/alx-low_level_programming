895e226aa07ddbae198545351f1e38cd43afe723
stevendegreat <stevendenjuguna@gmail.com>
2023-08-08 23:37:20 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,62 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-int i, d, n, len, f, digit;
-
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-
-while (s[len] != '\0')
-len++;
-
-while (i < len && f == 0)
-{
-if (s[i] == '-')
-++d;
-
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-f = 0;
-}
-i++;
-}
-
-if (f == 0)
-return n;
-
-/* Add a return statement if 'f' is not 0 (for safety)*/
-return n;
-}
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success) or 1 (Error)
+ ⦁main - multiplies two numbers
+ ⦁ 
+ @argc: number of arguments
+ ⦁@argv: array of arguments
+ ⦁ 
+ ⦁Return: always 0 (Success) or 1 (Error)
  */
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
+char *endptr;
 
 if (argc != 3)
 {
@@ -64,8 +21,20 @@ printf("Error\n");
 return 1;
 }
 
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
+num1 = strtol(argv[1], &endptr, 10);
+if (*endptr != '\0')
+{
+perror("Invalid argument");
+return 1;
+}
+
+num2 = strtol(argv[2], &endptr, 10);
+if (*endptr != '\0')
+{
+perror("Invalid argument");
+return 1;
+}
+
 result = num1 * num2;
 
 printf("%d\n", result);
