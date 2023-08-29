9e723e6bd09276c2a1e8782a3889886e0704fa8d
stevendegreat <stevendenjuguna@gmail.com>
2023-08-29 02:50:55 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all the elements of a listint_t list
-* @h: Pointer to the listint_t list to print
-* Return: The number of nodes printed
-*/
+ * function that prints all the elements of a listint_t list.
+ * print_listint - prints all the elements of a listint_t list
+ * @h: Pointer to the listint_t list to print
+ * Return: The number of nodes printed
+ */
 size_t print_listint(const listint_t *h)
 {
 size_t num = 0; /* Variable to store the number of nodes printed */
