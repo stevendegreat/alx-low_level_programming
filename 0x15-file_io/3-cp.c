02779a0932232ef17f8a2bd0ed3137d029c8c822
stevendegreat <stevendenjuguna@gmail.com>
2023-09-11 06:53:16 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
 void close_file(int fd);
+char *create_buffer(char *file);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
- *  a program that copies the content of a file to another file.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ *
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
@@ -30,7 +30,6 @@ return (buffer);
 /**
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
- * a program that copies the content of a file to another file.
  */
 void close_file(int fd)
 {
@@ -46,15 +45,16 @@ exit(100);
 }
 
 /**
+ Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
-* Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
+ *
  * Return: 0 on success.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
- *  a program that copies the content of a file to another file.
  */
 int main(int argc, char *argv[])
 {
@@ -63,7 +63,7 @@ char *buffer;
 
 if (argc != 3)
 {
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
