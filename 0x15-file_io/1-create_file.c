ff4fc5fcef59fa225e4cfe5c6364205882424e9c
stevendegreat <stevendenjuguna@gmail.com>
2023-10-02 10:10:23 -0700

    file input output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,8 @@
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  * coded by stevedegreat
- * Return: If the function fails - -1, Otherwise - 1.       
+ * Return: If the function fails - -1
+ *         Otherwise - 1.       
  */
 int create_file(const char *filename, char *text_content)
 {
